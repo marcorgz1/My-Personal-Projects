cfe9fa05ba5db799305617df363d2ca423746138
marcorgz1 <marco.valdemoro@gmail.com>
2026-02-02 16:13:13 +0100

    feat: start designing the UI of the translator app

--- Simple-Translator-App/src/App.jsx
+++ Simple-Translator-App/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
     <>
       <h1>Simple translator app</h1>
+      <section className='translation_panels__container'>
+        <div className="translation_origin__panel">
+          <select name="origin_language_selector" id="origin_language_selector">
+            <option value="">Example</option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+          </select>
+          <textarea name="to_translate_text" id="to_translate_text" placeholder='Escribe tu texto...' cols="30" rows="10"></textarea>
+        </div>
+        <div className="translation_destination__panel">
+          <select name="destination_language_selector" id="destination_language_selector">
+            <option value="">Example</option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+          </select>
+          <textarea name="translated_text" id="translated_text" placeholder='Escribe tu texto...' cols="30" rows="10"></textarea>
+        </div>
+      </section>
+      <button className='translate_button'>Traducir</button>
     </>
-  )
-}
+  );
+};
 
 export default App;
